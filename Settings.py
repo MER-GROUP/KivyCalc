c1bd85ed63247bf239c56f90f27615b44a9d8fd7
Maxim Ramanenka (Red Alert) <i@mer-group.ru>
2022-07-27 09:52:10 +0300

    add load_history save_history update_history

--- Settings.py
+++ Settings.py
@@ -20,17 +20,35 @@ class Settings:
         with open(file_settings, 'r') as file:
             return json.load(file)
     # ---------------------------------------------------------------------------
+    # загрузка истории файла json
+    def load_history(self, file_history: str=file_history) -> list:
+        # считываем из файла
+        with open(file_history, 'r') as file:
+            return json.load(file)
+    # ---------------------------------------------------------------------------
     # сохранение настроек файла json
     def save_settings(self, settings: dict, file_settings: str=file_settings) -> None:
         # записываем в файл данные в формате json
         with open(file_settings, 'w') as file:
             json.dump(settings, file, indent=4, sort_keys=True)
     # ---------------------------------------------------------------------------
+    # сохранение истории файла json
+    def save_history(self, history: list, file_history: str=file_history) -> None:
+        # записываем в файл данные в формате json
+        with open(file_history, 'w') as file:
+            json.dump(history, file, indent=4, sort_keys=True)
+    # ---------------------------------------------------------------------------
     # обновление настроек файла json
     def update_settings(self, **settings: dict) -> None:
         # записываем в файл данные в формате json
         with open(file_settings, 'w') as file:
             json.dump(settings, file, indent=4, sort_keys=True)
+    # ---------------------------------------------------------------------------
+    # обновление истории файла json
+    def update_history(self, *history: list) -> None:
+        # записываем в файл данные в формате json
+        with open(file_history, 'w') as file:
+            json.dump(history, file, indent=4, sort_keys=True)
     # ---------------------------------------------------------------------------
 # *****************************************************************************************
 # тесты
@@ -57,4 +75,22 @@ if __name__ == '__main__':
     print(settings.load_settings())
     print(settings.update_settings(round='3', hist='777', vibro='0.1'))
     print(settings.load_settings())
+
+    print('-------------------------------------')
+    print('+++++load_history+++++')
+
+    print(settings.load_history())
+
+    print('-------------------------------------')
+    print('+++++save_history+++++')
+
+    arr = settings.load_history()
+    arr.extend(['1', '2', '3'])
+    settings.save_history(arr)
+    print(settings.load_history())
+
+    print('-------------------------------------')
+    print('+++++update_history+++++')
+    settings.update_history('1', '898', '3')
+    print(settings.load_history())
 # *****************************************************************************************
